9af2cf10e0019b8dab8fa6b9c91abee7ef4580bb
Jeisson Benitez <123263559+Sieghart205@users.noreply.github.com>
2024-10-27 20:26:49 -0500

    mondongo

--- script/script.js
+++ script/script.js
@@ -1,13 +1,15 @@
-const sec4 = document.getElementById("sec-4");
-
-sec4.addEventListener("mousemove", e => {
-    let y = e.clientY;
-    let x = e.clientX;
-    const bg = document.getElementById("bg");
-
-    bg.style.top = `${y - 960}px`;
-    bg.style.left = `${x - 960}px`;
-});
-
 const outline = document.getElementById("outline");
 const cursor = document.getElementById("cursor")
+
+window.addEventListener("mousemove",e=>{
+    let x = e.clientX
+    let y = e.clientY;
+
+    setTimeout(()=>{
+        outline.style.top = `${y - 50}px`
+        outline.style.left = `${x - 50}px`
+    },50)
+
+    cursor.style.top = `${y - 15}px`
+    cursor.style.left = `${x - 15}px`
+})
